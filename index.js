f0a4c3595ffc20af7cfe4f419e1f50390528d6fd
Maicon <mviconoliveirv@gmail.com>
2024-12-13 21:14:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Hello World!!!");
+document.getElementById("divTeste").innerText
+="Texto";
+
+var numero1 = 2;
+var numero2 = 3;
+
+console.log(numero1 + numero2);
+
+numero1 = "2";
+console.log(numero2 + numero1);
+
+
+function somar() {
+    document.getElementById("divTeste").
+    innerText = document.getElementById
+    ("txtNumero1").value + document.getElementById
+    getElementById("txtNumero1").value) + 
+    parseInt(document.getElementById
+    ("txtNumero2").number)
+
+}
